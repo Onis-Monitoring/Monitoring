a1001b3a80e81104ce796a500b3f14664dc0ff3d
Edgar <edgar.lopez@onissolutions.com>
2020-06-30 13:14:28 -0500

    mongodb_exporter

--- start.sh
+++ start.sh
@@ -14,27 +14,28 @@ echo "Installing Grafana"
 kubectl create -f grafana-datasources.yaml
 kubectl create -f grafana-dashboards.yml
 kubectl create -f grafana-dashboards-definition.yaml
+kubectl create -f grafana-pvc.yaml
 kubectl create -f grafana-deployment-persist.yaml
 kubectl create -f grafana-service.yaml
 
-echo "Installing Alert Manager"
-kubectl create -f alertmanager-deployment.yaml
-kubectl create -f alertmanager-service.yaml
+# echo "Installing Alert Manager"
+# kubectl create -f alertmanager-deployment.yaml
+# kubectl create -f alertmanager-service.yaml
 
-echo "Installing kube-state"
-kubectl create -f kube-state/
+# echo "Installing kube-state"
+# kubectl create -f kube-state/
 
-echo "Installing Node-exporter"
-kubectl create -f node-exporter/
+# echo "Installing Node-exporter"
+# kubectl create -f node-exporter/
 
 echo "Installing mongodb-exporter"
 kubectl create -f mongodb-exporter/deployment.yaml
 # kubectl create -f mongodb-exporter/service.yaml
 
-echo "Installing snmp-exporter"
-kubectl create -f snmp-exporter/configmap.yaml
-kubectl create -f snmp-exporter/deployment.yaml
-kubectl create -f snmp-exporter/service.yaml
+# echo "Installing snmp-exporter"
+# kubectl create -f snmp-exporter/configmap.yaml
+# kubectl create -f snmp-exporter/deployment.yaml
+# kubectl create -f snmp-exporter/service.yaml
 
 # echo "Installing vmware-exporter"
 # kubectl create -f vmware-exporter/
